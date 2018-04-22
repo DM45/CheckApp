de4c8e87ae681ad0468daed5eac22b9c45026640
mda <mda.tomsk@gmail.com>
2018-04-22 23:14:03 +0700

    1234

--- project1/forms.py
+++ project1/forms.py
@@ -4,28 +4,46 @@ from django.contrib.auth.models import User
 
 
 class UpdateDocumentForm(forms.ModelForm):
+	ACTUAL = 'Актуально'
+	IRRELEVANT = 'Неактуально'
+	REWORK = 'На доработке'
+	CHECKING = 'На проверке'
+
+	DOCUMENT_STATUS = [ACTUAL, IRRELEVANT, REWORK, CHECKING]
+
 	class Meta:
 		model = Document
 		fields = ('name', 'department', 'last_success_check_date', 'document_file', 'status')
+		labels = {'name':'Название документа', 'department': 'ЦЭС',
+		'last_success_check_date': 'Дата последней успешной проверки', 'document_file': 'Файл',
+		'status': 'Статус'}
 
 		def __init__(self, *args, **kwargs):
+			groupe = kwargs.pop('groups')
 			super(UpdateDocumentForm, self).__init__(*args, **kwargs)
-			if request.user.groups == 'Controler':			
-				self.fields['status'].choices = ['Актуально', 'На доработке', 'Неактуально', 'В работе']
+			if groupe == 'Controler':
+				self.fields['status'].choices = DOCUMENT_STATUS
 
+
+
+
+			'''
+			self.user.groupe = kwargs.pop('groups')
+			super(UpdateDocumentForm).__init__(*args, **kwargs)
+			if groupe == 'Controler':			
+				self.fields['status'].choices = DOCUMENT_STATUS'''
+
+
+#self.fields['status'].choices = ['Actual', 'In work']
 '''
-class DocumentForm(forms.ModelForm):
-	class Meta:
-		models = Document
-		fields = ('name', 'department', 'last_check_date', 'check_period')
+	ACTUAL = 'Актуально'
+	IRRELEVANT = 'Неактуально'
+	REWORK = 'На доработке'
+	CHECKING = 'На проверке'
 
-#		def document_new(request):
-#    		if request.method == "POST":
-#        		form = DocumentForm(request.POST)
-#        		if form.is_valid():
-#            		document = form.save(commit=False)
-#            		post.save()
-#            	return redirect('post_detail', pk=post.pk)
-#    		else:
-#        		form = DocumentForm()
-#    	return render(request, 'blog/post_edit.html', {'form': form})'''
+	DOCUMENT_STATUS = [ACTUAL, IRRELEVANT, REWORK, CHECKING]
+	if request.user.groups != 'Controler':
+		status =  forms.ChoiceField(choices=[(x, x) for x in DOCUMENT_STATUS])
+'''
+
+
